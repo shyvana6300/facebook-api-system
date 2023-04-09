ca7512d50bc9d070733dffe28a7910b18c3e3d52
Hoang long <shyvana6300@gmaiil.com>
2023-04-10 02:31:55 +0700

    commit

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+var user = [
+    {
+        name: 'user1',
+        age: 12
+    },
+    {
+        name: 'user2',
+        age: 21
+    }
+]
 app.get('/', (req, res) => {
     console.log('New request has just called!');
     res.send('Welcome to API System');
@@ -8,9 +18,16 @@ app.get('/', (req, res) => {
 }
 );
 
+app.get('/user', (req, res) => {
+    console.log('--- Request: get list user ---');
+    res.send('User List: ', user);
+    res.end();
+}
+);
+
 app.post('/user/register', (req, res) => {
     console.log('--- Execute Create New Account ---')
-    res.send('Create new account!');
+    res.send();
     res.end();
 }
 );
