742d14ba843e06206d4eaaf78b6065ffc9cb51eb
nguyenhoanglong <nguyenhoanglong@luvina.net>
2023-04-13 18:39:30 +0700

    LongNH commit: fixing bug cesu encoding

--- test/api.test.js
+++ test/api.test.js
@@ -4,9 +4,10 @@ let server;
 describe('/api/facebook', () => {
     beforeEach(() => {server = require('../main');})
     afterEach(() => {server.close();});
+
     describe('GET /', () => {
-        it('should return the test result api', () => {
-            const res = request(server).get('/');
+        it('should return the test result api', async () => {
+            const res = await request(server).get('/');
             expect(res.status).toBe(200);
         });
     });
