a2210b474dde58caa8ac5038dce7dae37cf0a22d
nguyenhoanglong <nguyenhoanglong@luvina.net>
2023-04-18 09:32:59 +0700

    fix test case OK middleware

--- test/middleware.test.js
+++ test/middleware.test.js
@@ -6,26 +6,28 @@ describe("Test the accountValidator", () => {
     const validateAccount = accountValidator.validateAccount;
     describe("Test the validateAccount", () => {
         describe("Test caseOK", () => {
-        test("It should call the next() function", () => {
-            const mockedNext = jest.fn();
-            const mockedReq = mockValidateAccount.caseOK.mockReq;
-            const mockedRes = jest.fn();
-            const result = validateAccount(mockedReq, mockedRes, mockedNext);
-            expect(mockedNext).toHaveBeenCalled();
+            test("It should call the next() function", () => {
+                const mockedNext = jest.fn();
+                const mockedReq = mockValidateAccount.caseOK.mockReq;
+                const mockedRes = jest.fn();
+                const result = validateAccount(mockedReq, mockedRes, mockedNext);
+                expect(mockedNext).toHaveBeenCalled();
+            });
         });
-    });
-    describe("Test caseNG", () => {
-        test("It should return error message email required", () => {
-            const mockedNext = jest.fn();
-            const mockedReq = mockValidateAccount.caseNG.mockReq.missingEmail;
-            const mockedRes = {};
-            // TODO: xử lý mock function cho return res.status().send()
-            mockedRes.status = () => {
-                send: jest.fn();
-            }
-            const result = validateAccount(mockedReq, mockedRes, mockedNext);
-            expect(result.status(400).send).toHaveBeenCalledWith(`"email" is required`);
+
+        // TODO: xử lý mock function cho return res.status().send()
+        describe("Test caseNG", () => {
+            test("It should return error message email required", () => {
+                const mockedNext = jest.fn();
+                const mockedReq = mockValidateAccount.caseNG.mockReq.missingEmail;
+                const mockedRes = {};
+                
+                mockedRes.status = () => {
+                    send: jest.fn();
+                }
+                const result = validateAccount(mockedReq, mockedRes, mockedNext);
+                expect(result.status(400).send).toHaveBeenCalledWith(`"email" is required`);
+            });
         });
-    });
     });
 });
